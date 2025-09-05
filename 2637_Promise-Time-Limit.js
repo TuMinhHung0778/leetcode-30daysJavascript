c13f02f7fe0dc2f7b2ca0cbfb3855702aaf8cc26
TuMinhHung0778 <tuminhhung0901@gmail.com>
2025-09-05 14:12:58 +0700

    update 5/9

--- 2637_Promise-Time-Limit.js
+++ 2637_Promise-Time-Limit.js
@@ -124,6 +124,7 @@ var timeLimit = function (fn, t) {
 
 
 
+
 /**
  * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
  * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
